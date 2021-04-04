683d3ac1b196c9d9d5f4732db527d440baef6f39
Sahivy Gonzalez <52933755+sahivy13@users.noreply.github.com>
2021-04-04 03:26:54 -0400

    Delete explore.py

--- python_basics/Basics/explore.py
+++
@@ -1,9 +0,0 @@
-from twilio.rest import Client
-
-client = Client(
-    "AC740ce6c7348d5301b0a7c7189a0dbd10", # ACCOUNT
-    "cde6817141781b6c8557b60f3e83033a" # Token
-)
-
-for msg in client.messages.list():
-    print(msg.body)
